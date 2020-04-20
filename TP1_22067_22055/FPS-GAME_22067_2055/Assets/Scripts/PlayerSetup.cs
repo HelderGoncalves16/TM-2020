0e9d07e89c98240f3e90996be604e9b357543892
Hélder Gonçalves <46654418+HelderGoncalves16@users.noreply.github.com>
2020-04-20 16:44:07 +0100

    Novo Update
    Novo Update do Trabalho - Feito Pelo Pedro

---
+++ TP1_22067_22055/FPS-GAME_22067_2055/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+
+[RequireComponent(typeof(Player))]
+public class PlayerSetup : NetworkBehaviour
+{
+
+    [SerializeField]
+    private Behaviour[] componentsToDisable;
+
+    [SerializeField]
+    string remoteLayerName = "RemotePlayer";
+
+    Camera sceneCamara;
+
+    void Start()
+    {
+        //desativar components que devem estar ativos apenas no jogador que controlamos
+        if (!isLocalPlayer)
+        {
+            DisableComponents();
+            AssignRemoteLayer();
+        }
+        else
+        {
+            //somos o player local - desativar a scene camera
+            sceneCamara = Camera.main;
+            if (sceneCamara != null)
+            {
+                sceneCamara.gameObject.SetActive(false);
+            }
+        }
+
+        GetComponent<Player>().Setup();
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        string _netID = GetComponent<NetworkIdentity>().netId.ToString();
+        Player _player = GetComponent<Player>();
+
+        GameManager.RegisterPlayer(_netID, _player);
+    }
+
+
+    void AssignRemoteLayer()
+    {
+        gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+    }
+
+    void DisableComponents()
+    {
+        for (int i = 0; i < componentsToDisable.Length; i++)
+        {
+            componentsToDisable[i].enabled = false;
+        }
+    }
+
+    void OnDisable()
+    {
+        if (sceneCamara != null)
+        {
+            sceneCamara.gameObject.SetActive(true);
+        }
+
+        GameManager.UnRegisterPlayer(transform.name);
+    }
+
+}
